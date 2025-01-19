5d8cbe92f4ddf9999e2ea9c36ab3ac2500d98b04
theccz <ccaprusu@gmail.com>
2025-01-19 18:23:40 +0100

    Update azurerm version in terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,8 +3,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.12"
-    }
+      version = "~> 3.105"
+}
     modtm = {
       source  = "Azure/modtm"
       version = "~> 0.3"
